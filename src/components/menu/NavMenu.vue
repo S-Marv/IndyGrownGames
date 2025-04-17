aa4dea540630f2f5f62ee267ca2e40f901ba65f1
coreybrowndev <bcorey395@gmail.com>
2025-04-17 15:37:35 -0400

    Setup menu dropdown component

---
+++ src/components/menu/NavMenu.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+import { Menu } from "primevue";
+import { Button } from "primevue";
+
+const menu = ref();
+const items = ref([
+  {
+    label: "Options",
+    items: [
+      {
+        label: "Refresh",
+        icon: "pi pi-refresh",
+      },
+      {
+        label: "Export",
+        icon: "pi pi-upload",
+      },
+    ],
+  },
+]);
+
+const toggle = (event) => {
+  menu.value.toggle(event);
+};
+</script>
+
+<template>
+  <div class="card flex justify-center">
+    <Button type="button" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu">
+      <img src="/menuBar/DropDown.svg" width="40px" height="40px" />
+    </Button>
+    <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
+  </div>
+</template>
--- src/components/menuBar/MenuBar.vue
+++ src/components/menuBar/MenuBar.vue
@@ -1,32 +1,34 @@
 <script setup>
-import Menubar from 'primevue/menubar';
-import { Button } from 'primevue';
+import Menubar from "primevue/menubar";
+import NavMenu from "../menu/NavMenu.vue";
+import { Button } from "primevue";
 </script>
 
 <template>
-<Menubar :model="[]">
+  <Menubar :model="[]">
     <template #start>
-		<a href="/">
-			<img src="/menuBar/Icon.svg" width="50px" height="50px"/>
-			<img src="/menuBar/IndyGrownGames.svg" width="400"/>
-		</a>
+      <a href="/">
+        <img src="/menuBar/Icon.svg" width="50px" height="50px" />
+        <img src="/menuBar/IndyGrownGames.svg" width="400" />
+      </a>
     </template>
     <template #end>
-        <Button>
-			<img src="/menuBar/DropDown.svg" width="40px" height="40px"/>
-		</Button>
+      <NavMenu />
+      <!-- <Button>
+        <img src="/menuBar/DropDown.svg" width="40px" height="40px" />
+      </Button> -->
     </template>
-</Menubar>
+  </Menubar>
 </template>
 
 <style scoped>
-.p-menubar{
-	border-radius: 0;
-	background-image: linear-gradient(to right, #67CCFF , #A98CEE, #FF39D7, #F5FF6C)
+.p-menubar {
+  border-radius: 0;
+  background-image: linear-gradient(to right, #67ccff, #a98cee, #ff39d7, #f5ff6c);
 }
 
-Button{
-	background-color: transparent;
-	border-color: transparent;
+Button {
+  background-color: transparent;
+  border-color: transparent;
 }
-</style>	
+</style>
