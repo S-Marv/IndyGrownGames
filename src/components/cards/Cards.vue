7c761bffbdf1e52daec81e19bdd1879a7c984a37
Samuel Marvin <97563317+SLMarv@users.noreply.github.com>
2025-04-17 20:48:52 -0400

    Change groups to cards in edu and games

---
+++ src/components/cards/Cards.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { Card } from 'primevue';
+
+const width = "250px";
+defineProps({
+	cards:Array,
+})
+
+</script>
+
+<template>
+	<div class="card-container">
+		<Card :style="'width:' + width + ';'" v-for="card in cards">
+			<template #header>
+				<img :alt="card.name" :src="card.icon.imageURL" height="100px" :width="width"/>
+			</template>
+			<template #title><a :href="card.icon.link">{{card.name}}</a></template>
+			<template #content>
+				<p class="m-0">
+					{{card.text}}
+				</p>
+			</template>
+		</Card>
+	</div>
+</template>
+
+<style scoped>
+.card-container{
+	display:flex;
+	flex-flow:wrap;
+	justify-self: center;
+
+}
+
+.p-card{
+	margin: 0px 10px 10px;
+	overflow: hidden;
+	border: medium solid;
+}
+
+img{
+	object-fit: cover;
+}
+</style>
--- src/views/EducationView.vue
+++ src/views/EducationView.vue
@@ -1,12 +1,18 @@
 <script setup>
-import Group from '@/components/groups/Group.vue';
+import Cards from '@/components/cards/Cards.vue';
+import { CardData } from '@/components/cards/CardData';
+import { Icon } from '@/objects/Icon';
+const bsu = new CardData("Ball State University",
+		new Icon("education/ballstate.jpg", "https://www.bsu.edu/academics/collegesanddepartments/computer-science/academic-programs/majors/game-design"),
+		"BSU has a game design program.")
+
+const cards = [
+	bsu, bsu, bsu, bsu, bsu, bsu, bsu
+]
 
 </script>
 
 
 <template>
-<Group title="Ball State University" img-on-left imageUrl="education/ballstate.jpg">
-BSU has a game design program.
-</Group>
+<Cards :cards="cards"/>
 </template>
-
