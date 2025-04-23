8915063b241996e5c534f1cb89c1a8879073e7e9
Samuel Marvin <97563317+SLMarv@users.noreply.github.com>
2025-04-23 12:49:53 -0400

    Fix typos in group texts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@ import SectionHeader from '@/components/SectionHeader.vue';
 const groups = ref([
   {
     title: "IGDA Indianapolis",
-    text: "We’re a friendly group of game makers who get together once a month to talk about game development and share best practices. We’re your support group when something isn’t working, when you need to find an artist, or when you’re hyped about something you made and what to share it with your peers. This organization is for you to make connections and find opportunity.",
+    text: "We’re a friendly group of game makers who get together once a month to talk about game development and share best practices. We’re your support group when something isn’t working, when you need to find an artist, or when you’re hyped about something you made and want to share it with your peers. This organization is for you to make connections and find opportunity.",
     imageUrl: "groups/igda.png",
     galleriaImage: "galleria/IGDA Pic.png",
     icons:[new Icon("groups/discord.png", "https://discord.com/")],
@@ -27,7 +27,7 @@ const groups = ref([
   },
   {
     title: "Indy Tabletop Game Creators",
-    text: "Indy TableTop Game Creators is a group based out of central Indiana that helps game designers and playtesters create great tabletop games together. We’re largely both “Indy” (Indianapolis) and “indie” (independent), but anyone who wants to help and be helped is welcome. Beginners are welcome! Early hand-scribbled prototypes are encouraged!",
+    text: "Indy TableTop Game Creators is a group based out of central Indiana that helps game designers and playtesters create great tabletop games together. We’re largely both “Indy” (Indianapolis) and “indie” (independent), but anyone who wants to help and be helped is welcome. Beginners are welcome! Early, hand-scribbled prototypes are encouraged!",
     imageUrl: "groups/IndyGameCreators.svg",
     galleriaImage: "galleria/ITTC.png",
     icons:[new Icon("groups/discord.png", "https://discord.com/"), new Icon("groups/insta.png", "https://google.com")],
